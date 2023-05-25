a7ad2c02a2f97dc8fd1c2a86ed5548e38faa370f
Michael Wagner <mdwranger@gmail.com>
2023-05-24 23:17:11 -0400

    feat: refactor folder

--- src/lucky_template.cr
+++ src/lucky_template.cr
@@ -4,41 +4,103 @@ require "./lucky_template/*"
 module LuckyTemplate
   extend self
 
+  alias Snapshot = Hash(String, FileSystem)
+
   def create_parent_folder(& : Folder ->) : Folder
-    parent_folder = Folder.new
-    parent_folder.lock do
-      yield parent_folder
+    Folder.new.tap do |folder|
+      folder.lock do
+        yield folder
+      end
     end
-    parent_folder
   end
 
+  # Writes the folder to disk at the given path
+  #
+  # Raises `LuckyTemplate::Error` if folder is locked
   def write!(location : Path, folder : Folder) : Nil
     Dir.mkdir_p(location)
-    folder.write_to_disk!(location)
+    if folder.locked?
+      raise Error.new("Cannot write to disk if folder is locked")
+    end
+    write_folder!(location, folder)
+  end
+
+  # NOTE: Recursive
+  private def write_folder!(prev_path : Path, folder : Folder) : Nil
+    folder.files.each do |name, file|
+      path = prev_path / name
+      case file
+      in File
+        ::File.open(path, "w") do |io|
+          file.to_s(io)
+        end
+      in Folder
+        Dir.mkdir_p(path)
+        write_folder!(path, file)
+      end
+    end
   end
 
   # Same as `#create_parent_folder` and `#write!`
   def write!(location : Path, & : Folder ->) : Folder
-    folder = create_parent_folder do |parent_folder|
-      yield parent_folder
+    create_parent_folder do |folder|
+      yield folder
+    end.tap do |folder|
+      write!(location, folder)
     end
-    write!(location, folder)
-    folder
   end
 
-  # Validates _folder_ at _location_ contains the same files and folders
+  # Returns `true` if the folder is _valid_ at the given path
+  #
+  # _valid_ - Files and folder exist within the given path
   #
   # Raises `::File::NotFoundError` if either a file or folder does not exist
   def validate!(location : Path, folder : Folder) : Bool
-    folder.validate!(location)
+    snapshot_files(folder).each do |filepath, type|
+      path = location / filepath
+      case type
+      in .file?
+        ::File.size(path)
+      in .folder?
+        Dir.open(path) { }
+      end
+    end
+    true
   end
 
-  # Validates _folder_ at _location_ contains the same files and folders
+  # Returns a `Bool` if the folder is _valid_ at the given path
+  #
+  # _valid_ - Files and folder exist within the given path
   def validate?(location : Path, folder : Folder) : Bool
-    folder.validate?(location)
+    validate!(location, folder)
+  rescue
+    false
   end
 
-  def snapshot_files(folder : Folder)
-    folder.snapshot_files
+  # Returns a new `Snapshot` of all files and folders within this folder
+  #
+  # Raises `LuckyTemplate::Error` if folder is locked
+  def snapshot_files(folder : Folder) : Snapshot
+    if folder.locked?
+      raise Error.new("Cannot get snapshot if folder is locked")
+    end
+    Snapshot.new.tap do |snapshot|
+      snapshot_folder(Path.new, folder, snapshot)
+    end
+  end
+
+  # NOTE: Recursive
+  private def snapshot_folder(prev_path : Path, folder : Folder, snapshot : Snapshot) : Nil
+    folder.files.each do |name, file|
+      path = prev_path / name
+      key = path.to_posix.to_s
+      case file
+      in File
+        snapshot[key] = FileSystem::File
+      in Folder
+        snapshot[key] = FileSystem::Folder
+        snapshot_folder(path, file, snapshot)
+      end
+    end
   end
 end
--- src/lucky_template/folder.cr
+++ src/lucky_template/folder.cr
@@ -1,36 +1,90 @@
 module LuckyTemplate
   class Folder
     alias Files = File | Folder
-    alias Snapshot = Hash(String, FileSystem)
 
     @files = {} of String => Files
     @locked = false
 
-    # Adds a new `File` to the folder with static _content_
+    # Adds a new `File` to the folder with _static_ *content*
+    #
+    # Examples:
+    #
+    # ```
+    # add_file("hello.txt", "hello world")
+    # ```
+    #
+    # ```
+    # add_file("hello.txt", <<-TEXT)
+    # hello world
+    # TEXT
+    # ```
     def add_file(name : String, content : String) : self
-      insert_file(name, File.new(content))
+      add_file(name, File.new(content))
     end
 
     # Adds a new `File` to the folder with `Fileable` interface
+    #
+    # Example:
+    #
+    # ```
+    # class Hello
+    #   include LuckyTemplate::Fileable
+    # end
+    #
+    # add_file("hello.txt", Hello.new)
+    # ```
     def add_file(name : String, klass : Fileable) : self
-      insert_file(name, File.new(klass))
+      add_file(name, File.new(klass))
     end
 
     # Adds a new `File` to the folder yielding an `IO`
+    #
+    # Example:
+    #
+    # ```
+    # add_file("hello.txt") do |io|
+    #   ECR.embed("hello.ecr", io)
+    # end
+    # ```
     def add_file(name : String, &block : FileProc) : self
-      insert_file(name, File.new(block))
+      add_file(name, File.new(block))
     end
 
     # Adds a new empty `File` to the folder
+    #
+    # Example:
+    #
+    # ```
+    # add_file("hello.txt")
+    # ```
     def add_file(name : String) : self
-      insert_file(name, File.new(nil))
+      add_file(name, File.new(nil))
     end
 
-    private def insert_file(name : String, file : File) : self
+    # Adds a `File` to the folder
+    def add_file(name : String, file : File) : self
       @files[name] = file
       self
     end
 
+    # Adds nested folders, yielding the last one
+    #
+    # Example:
+    #
+    # ```
+    # add_folder("a", "b", "c") do |folder| # folder == "c"
+    #   folder.add_file("hello.txt")
+    # end
+    # ```
+    #
+    # Produces these folder paths:
+    #
+    # ```text
+    # a
+    # a/b
+    # a/b/c
+    # a/b/c/hello.txt
+    # ```
     def add_folder(*names : String, & : Folder ->) : self
       prev : Folder? = nil
       names.each_with_index do |name, index|
@@ -50,23 +104,51 @@ module LuckyTemplate
       self
     end
 
+    # Adds nested empty folders
+    #
+    # Example:
+    #
+    # ```
+    # add_folder("a", "b", "c", "d")
+    # ```
+    #
+    # Produces these folder paths:
+    #
+    # ```text
+    # a
+    # a/b
+    # a/b/c
+    # a/b/c/d
+    # ```
     def add_folder(*names : String) : self
       add_folder(*names) { }
     end
 
+    # Insert an existing folder
+    #
+    # Raises `LuckyTemplate::Error` if one of the following are true:
+    #   - existing folder is equal to itself
+    #   - existing folder is locked
+    #
+    # Example:
+    #
+    # ```
+    # another_folder = LuckyTemplate::Folder.new
+    # LuckyTemplate.create_parent_folder do |folder|
+    #   folder.insert_folder(another_folder)
+    # end
+    # ```
     def insert_folder(name : String, folder : Folder) : self
       if folder == self
         raise Error.new("Cannot add Folder equal to itself")
       elsif folder.locked?
-        raise Error.new("Cannot add Folder that is already being yielded")
+        raise Error.new("Cannot add Folder that is locked")
       end
       @files[name] = folder
       self
     end
 
-    # Locks the folder for as long as block yields
-    #
-    # To be used as a safe-guard to protect against circular references.
+    # To be used as a safe-guard to protect against circular references
     protected def lock(&) : Nil
       @locked = true
       yield
@@ -74,89 +156,13 @@ module LuckyTemplate
       @locked = false
     end
 
-    # TODO: write description
+    # Checks if folder is _locked_
     def locked? : Bool
       @locked
     end
 
     protected def files
       @files
-    end
-
-    # Writes the folder to disk at the given path
-    protected def write_to_disk!(path : Path) : Nil
-      if locked?
-        raise Error.new("Cannot write to disk while being yielded")
-      end
-      write_folder_to_disk!(path, self)
-    end
-
-    # NOTE: Recursive
-    private def write_folder_to_disk!(prev_path : Path, folder : Folder) : Nil
-      folder.files.each do |name, file|
-        path = prev_path / name
-        case file
-        in File
-          ::File.open(path, "w") do |io|
-            file.to_s(io)
-          end
-        in Folder
-          Dir.mkdir_p(path)
-          write_folder_to_disk!(path, file)
-        end
-      end
-    end
-
-    # Returns `true` if the folder is _valid_ at the given path
-    #
-    # _valid_ - Files and folder exist within the given path
-    #
-    # Raises `::File::NotFoundError` if either a file or folder does not exist
-    protected def validate!(location : Path) : Bool
-      snapshot_files.each do |filepath, type|
-        path = location / filepath
-        case type
-        in .file?
-          ::File.size(path)
-        in .folder?
-          Dir.open(path) { }
-        end
-      end
-      true
-    end
-
-    # Returns a `Bool` if the folder is _valid_ at the given path
-    #
-    # _valid_ - Files and folder exist within the given path
-    protected def validate?(location : Path) : Bool
-      validate!(location)
-    rescue
-      false
-    end
-
-    # Returns a new `Snapshot` of all files and folders within this folder
-    protected def snapshot_files : Snapshot
-      if locked?
-        raise Error.new("Cannot get snapshot while being yielded")
-      end
-      Snapshot.new.tap do |snapshot|
-        snapshot_folder(Path.new, self, snapshot)
-      end
-    end
-
-    # NOTE: Recursive
-    private def snapshot_folder(prev_path : Path, folder : Folder, snapshot : Snapshot) : Nil
-      folder.files.each do |name, file|
-        path = prev_path / name
-        key = path.to_posix.to_s
-        case file
-        in File
-          snapshot[key] = FileSystem::File
-        in Folder
-          snapshot[key] = FileSystem::Folder
-          snapshot_folder(path, file, snapshot)
-        end
-      end
     end
   end
 end
